3a8331cba7360bec4fcd88fa338768fd0b10224c
Joshua Perry <josh@6bit.com>
2023-12-02 10:42:46 -0700

    Refactor to use positional tuples

--- one/src/main.rs
+++ one/src/main.rs
@@ -8,11 +8,17 @@ fn read_lines(file: &str) -> Vec<String> {
         .collect()
 }
 
-fn extract_value(line: &String) -> i32 {
-    let digits = line
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<Vec<char>>();
+fn extract_digits(line: &String) -> Vec<(usize, &str)> {
+    line
+        .match_indices(|c: char| c.is_digit(10))
+        .collect()
+}
+
+fn extract_posvalue(chars: Vec<(usize, &str)>) -> i32 {
+    let digits = chars
+        .iter()
+        .map(|c| c.1)
+        .collect::<Vec<&str>>();
     let value = match digits[..] {
         [] => String::from("0"),
         [first] => format!("{first}{first}"),
@@ -21,25 +27,32 @@ fn extract_value(line: &String) -> i32 {
     value.parse::<i32>().unwrap()
 }
 
-fn extract_values(lines: &Vec<String>) -> Vec<i32> {
+fn extract_digit_values(lines: &Vec<String>) -> Vec<i32> {
     lines
-        .into_iter()
-        .map(extract_value)
+        .iter()
+        .map(extract_digits)
+        .map(extract_posvalue)
         .collect()
 }
 
 fn main() {
     let lines = read_lines("one/input");
-    let values = extract_values(&lines);
+    let values = extract_digit_values(&lines);
     let sum:i32 = values
         .into_iter()
         .sum();
     println!("calibration time, come on: {sum}");
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn extract_digit_value(line: &String) -> i32 {
+        let digits = extract_digits(&line);
+        extract_posvalue(digits)
+    }
 
     #[test]
     fn reads_lines() {
@@ -49,31 +62,31 @@ mod tests {
 
     #[test]
     fn extracts_multi_13_value() {
-        let value = extract_value(&String::from("abc1efg2hij3klm"));
+        let value = extract_digit_value(&String::from("abc1efg2hij3klm"));
         assert_eq!(value, 13);
     }
 
     #[test]
     fn extracts_multi_15_value() {
-        let value = extract_value(&String::from("abc1efg3hij5klm"));
+        let value = extract_digit_value(&String::from("abc1efg3hij5klm"));
         assert_eq!(value, 15);
     }
 
     #[test]
     fn extracts_double_value() {
-        let value = extract_value(&String::from("abc1ef3g"));
+        let value = extract_digit_value(&String::from("abc1ef3g"));
         assert_eq!(value, 13);
     }
 
     #[test]
     fn extracts_single_value() {
-        let value = extract_value(&String::from("abc1efg"));
+        let value = extract_digit_value(&String::from("abc1efg"));
         assert_eq!(value, 11);
     }
 
     #[test]
     fn extracts_no_value() {
-        let value = extract_value(&String::from("abcefg"));
+        let value = extract_digit_value(&String::from("abcefg"));
         assert_eq!(value, 0);
     }
 }
